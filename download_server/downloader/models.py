dfa15657b736855c0a11389dba23ddc508e144e2
Andrew <iiotep9huy@gmail.com>
2013-01-26 12:20:17 +0400

    SimpleUrlDownload is working now. Downloads lists shows current downloads.

--- download_server/downloader/models.py
+++ download_server/downloader/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
+import urllib2
+import sys
+
 # Create your models here.
 
 class Download(models.Model):
@@ -10,16 +13,62 @@ class Download(models.Model):
 	class Meta:
 		abstract = True
 
+
+	@classmethod
+	def create(Class, url):
+		download = Class()
+		download.url = url
+		download.name = url.split("/")[-1]
+		return download
+
 	def type(self):
 		pass
 
 	def __unicode__(self):
 		return self.name
 
+	def start(self):
+		pass
+
+	def pause(self):
+		pass
+
+	def stop(self):
+		pass
+
+	def resume(self):
+		pass
+
 class SimpleUrlDownload(Download):
 	
 	def type(self):
 		return "Simple Url Download"
+
+	def start(self):
+		file_name = "/tmp/" + self.name
+		u = urllib2.urlopen(self.url)
+		f = open(file_name, 'wb')
+		meta = u.info()
+		file_size = int(meta.getheaders("Content-Length")[0])
+		print >> sys.stderr, "Downloading: {0} Bytes: {1}".format(self.url, file_size)
+
+		downloaded_file_size = 0
+		read_block_size = 8196
+
+		#while buffer = u.read(block_size):
+		while True:
+			buffer = u.read(read_block_size)
+			if not buffer:
+				break
+
+			downloaded_file_size += len(buffer)
+			f.write(buffer)
+			download_progress = float(downloaded_file_size) / file_size
+			status = r"{0}  [{1:.2%}]".format(downloaded_file_size, download_progress)
+			status = status + chr(8)*(len(status) + 1)
+			print >> sys.stderr, status
+
+		print >> sys.stderr, "Download finished"
 
 class TorrentDownload(Download):
 
--- download_server/downloader/views.py
+++ download_server/downloader/views.py
@@ -3,6 +3,9 @@
 from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render
 from django.core.urlresolvers import reverse
+from models import Download
+from models import SimpleUrlDownload
+import sys
 
 def index(request):
 	return render(request, "downloader/index.html")
@@ -11,7 +14,29 @@ def new_download(request):
 	return render(request, "downloader/new_download.html")
 
 def submit_new_download(request):
+	try:
+		download_url = request.POST['download_url']
+		download_type = request.POST['download_type']
+	except (KeyError):
+		return render(request, 'downloader/new_download.html', {
+			'error_message': "You didn't select a download type.",
+		})
+	print >> sys.stderr, download_type
+
+	if download_type == "simple_url_download":
+		download = SimpleUrlDownload.create(download_url)
+	else:
+		return render(request, 'downloader/new_download.html', {
+			'error_message': "Unsupported download type.",
+		})
+	download.save()
+	download.start()
+
 	return HttpResponseRedirect(reverse('downloader:downloads_list'))
 
 def downloads_list(request):
-	return HttpResponse("Downloads list");
+	#latest_poll_list = Poll.objects.filter(pub_date__lte = timezone.now).order_by('-pub_date')[:5]
+	#latest_downloads_list = Download.objects.all()
+	latest_downloads_list = SimpleUrlDownload.objects.all()
+	context = {'latest_downloads_list': latest_downloads_list}
+	return render(request, "downloader/downloads_list.html", context)
